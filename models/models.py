c3585aa0810a80a865092d6faf9e4aced60b184e
fatiha <fatihaafalah1997@gmail.com>
2020-02-29 13:54:13 -0800

    [Add smart button facture]Modify

--- models/models.py
+++ models/models.py
@@ -8,13 +8,11 @@ class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'Openacademy Courses'
 
-    us=fields.Char(string="User",default=lambda self:self.env.user)
+    #user=fields.Char(string="User",default=lambda self:self.env.user)
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     responsible_id = fields.Many2one('res.users',ondelete='set null', string="Responsible", index=True)
     session_ids = fields.One2many('openacademy.session','course_id', string="Sessions")
-
-
 
     def copy(self, default=None):
         default = dict(default or {})
@@ -29,7 +27,6 @@ class Course(models.Model):
             new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
         default['name'] = new_name #?????
         return super(Course, self).copy(default)#????//????/
-
     _sql_constraints = [
         ('name_description_check',
          'CHECK(name != description)',
@@ -39,7 +36,6 @@ class Course(models.Model):
          'UNIQUE(name)',
          "The course title must be unique"),
     ]
-
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -52,17 +48,12 @@ class Session(models.Model):
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
     color = fields.Integer()
-    instructor_id = fields.Many2one('res.partner', string="Instructor",
-                                    domain=['|', ('instructor', '=', True),
-                                            ('category_id.name', 'ilike', "Teacher")])
-
-    course_id= fields.Many2one('openacademy.course',ondelete='cascade', string="Course", required=True)
+    instructor_id = fields.Many2one('res.partner', string="Instructor",domain=['|', ('instructor', '=', True),('category_id.name', 'ilike', "Teacher")])
+    course_id = fields.Many2one('openacademy.course',ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
-    end_date = fields.Date(string="End Date", store=True,
-        compute='_get_end_date', inverse='_set_end_date')
-    attendees_count = fields.Integer(
-       string="Attendees count", compute='_get_attendees_count', store=True)
+    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+    attendees_count = fields.Integer(string="Attendees count", compute='_get_attendees_count', store=True)
     price_per_hour = fields.Integer(help="Price")
     total = fields.Integer(help="total", compute='calc_total')
     price_session = fields.Integer(string="Price for session")
@@ -72,11 +63,35 @@ class Session(models.Model):
         ('confirm', "CONFIRM"),
         ('validate', "VALIDATE"),
     ], default='draft', string='State')
-    button_clicked = fields.Boolean(string='Button clicked')
-
+    button_clicked = fields.Boolean(string='facturee')
+    invoice_ids = fields.One2many("account.move", "session_id")
+    invoice_count = fields.Integer(string="count invoice", compute="_compute_invoice_count")
     # This function is triggered when the user clicks on the button 'Set to started'
 
+    def _compute_invoice_count(self):
+        self.invoice_count = self.env['account.move'].search_count([('session_id', '=', self.id)])
 
+    def action_view_invoice(self):
+        invoices = self.mapped('invoice_ids')
+        action = self.env.ref('account.action_move_out_invoice_type').read()[0]
+        if len(invoices) > 1:
+            action['domain'] = [('id', 'in', invoices.ids)]
+        elif len(invoices) == 1:
+            form_view = [(self.env.ref('account.view_move_form').id, 'form')]
+            if 'views' in action:
+                action['views'] = form_view + [(state, view) for state, view in action['views'] if view != 'form']
+            else:
+                action['views'] = form_view
+            action['res_id'] = invoices.id
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+
+        context = {
+            'default_type': 'out_invoice',
+        }
+
+        action['context'] = context
+        return action
     def confirm_progressbar(self):
         self.write({
             'state': 'confirm'
@@ -86,27 +101,21 @@ class Session(models.Model):
         self.button_clicked = True
         #data= les donnes envoyes au facturaion
         data = {
+            'session_id': self.id,#???/????
             'partner_id': self.instructor_id.id,
             'type': 'in_invoice',
             # 'partner_shipping_id' : self.instructor_id.address,
             'invoice_date': self.date,
-            # 'amount_total_signed': self.total
         }
         invoice = self.env['account.move'].create(data)
-
-
-
 
     # This function is triggered when the user clicks on the button 'Done'
     def valide_progressbar(self):
         self.write({
             'state': 'validate',
         })
-
     def calc_total(self):
         self.total = self.duration * self.price_per_hour
-
-
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
         for r in self:
